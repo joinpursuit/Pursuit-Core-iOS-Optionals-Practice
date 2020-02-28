86a538a02777ac94f9596a5155cbc7a236c84ee3
Ben Stone <benjamin.h.stone@gmail.com>
2020-02-28 16:40:35 -0500

    update naming and add print statements

--- OptionalsPractice.playground/Contents.swift
+++ OptionalsPractice.playground/Contents.swift
@@ -1,10 +1,8 @@
 import UIKit
 
-var str = "Hello, playground"
-
 // 1. Write a function that takes in an array of optional integers and returns an array with the optionals removed
 
-func removeOptionals(from: [Int?]) -> [Int] {
+func removeOptionals(from arr: [Int?]) -> [Int] {
     return []
 }
 
@@ -14,11 +12,12 @@ func removeOptionals(from: [Int?]) -> [Int] {
 //assert(removeOptionals(from: [1,2, nil ,3,nil]) == [1,2,3])
 //assert(removeOptionals(from: [10,nil,1,2, nil ,3,nil]) == [10,1,2,3])
 //assert(removeOptionals(from: [nil,nil,nil]) == [])
+//print("All tests passed for 'removeOptionals(from:)'")
 
 
 // 2. Write a function that takes in an Int? and a closure of type (Int) -> Int.  If the Int is not nil, then call the closure with it and return the result.  If the Int is nil, return nil.
 
-func manipulate(_ number: Int?, using: (Int) -> Int) -> Int? {
+func manipulate(_ number: Int?, using transform: (Int) -> Int) -> Int? {
     return -1
 }
 
@@ -33,22 +32,24 @@ func manipulate(_ number: Int?, using: (Int) -> Int) -> Int? {
 //let i3 = Optional<Int>.none
 //let o3 = manipulate(i3, using: { $0 / 2 })
 //assert(o3 == nil)
+//print("All tests passed for 'manipulate(_:using:)'")
 
 // 3. Write a function that takes in an array of Strings and returns an array of Ints.  If a string can't be converted to an Int, ignore it.
 
 func convertToInts(arr: [String]) -> [Int] {
-    return []
+    return [-1]
 }
 
 //assert(convertToInts(arr: ["11377", "11101", "11373", "10014", "10003", "11223"]) == [11377, 11101, 11373, 10014, 10003, 11223])
 //assert(convertToInts(arr: ["11377", "hello!", "11101", "11373", "10014", "10003", "goodbye!", "11223"]) == [11377, 11101, 11373, 10014, 10003, 11223])
 //assert(convertToInts(arr: ["12.32", "11377", "11101", "11373", "10014", "10003", "11223"]) == [11377, 11101, 11373, 10014, 10003, 11223])
 //assert(convertToInts(arr: ["999999999999999999999999999", "11377", "11101", "11373", "10014", "10003", "11223"]) == [11377, 11101, 11373, 10014, 10003, 11223])
+//print("All tests passed for 'convertToInts(arr:)'")
 
 
 // 4. Write a function that returns an array of all the ints in the unusal input below:
 
-func allInts(in: ([Int?], Int?, [[Int]?])?) -> [Int] {
+func allInts(in tuple: ([Int?], Int?, [[Int]?])?) -> [Int] {
     return [-1]
 }
 
@@ -60,7 +61,7 @@ func allInts(in: ([Int?], Int?, [[Int]?])?) -> [Int] {
 //
 //let unusualTupleThree: ([Int?], Int?, [[Int]?])? = ([nil, 1], 9, [[4], nil, nil, nil, [2]])
 //assert(allInts(in: unusualTupleThree) == [1,9,4,2])
-
+//print("All tests passed for 'allInts(in:)'")
 
 // 5. Write a function that Takes in an Int??? and returns either nil or the nested Int
 
@@ -71,9 +72,10 @@ func value(from num: Int???) -> Int? {
 //assert(value(from: nil) == nil)
 //assert(value(from: 3) == 3)
 //let nestedOptionalOne: Int??? = Optional<Int??>.some(Optional<Int?>.some(Optional<Int>.none))
-//assert(nestedOptionalOne == nil)
+//assert(value(from: nestedOptionalOne) == nil)
 //let nestedOptionalTwo: Int??? = Optional<Int??>.some(Optional<Int?>.some(Optional<Int>.some(10)))
-//assert(nestedOptionalTwo!!! == 10)
+//assert(value(from: nestedOptionalTwo!!!) == 10)
 //let nestedOptionalThree: Int??? = Optional<Int??>.some(Optional<Int?>.none)
-//assert(nestedOptionalThree == nil)
-
+//assert(value(from: nestedOptionalThree) == nil)
+//print("All tests passed for 'value(from:)'")
+//print("All tests passed")
